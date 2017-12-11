fc36844d0223b9dcaf529dc9875a4795b198b6fd
Király Péter <kirunews@gmail.com>
2017-12-11 07:02:06 +0100

    Add helper files.

--- europeana-qa-client-rest/build-all.sh
+++ europeana-qa-client-rest/build-all.sh
@@ -1,8 +1,15 @@
 CWD=`pwd`
+echo Build Metadata QA API
 cd ~/git/metadata-qa-api/
-mvn clean install
+git pull
+mvn clean install -Ptravis
+
+echo Build Europeana QA API
 cd ~/git/europeana-qa-api/
-mvn clean install
+git pull
+mvn clean install -Ptravis
+
+echo Build client
 cd $CWD
 mvn clean install -DskipTests
 
